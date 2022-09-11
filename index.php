14b2f298e4a02319f3ea402ed2b1d474e48f8154
DM Badhon <deathmountain20@yahoo.com>
2022-09-11 12:47:49 +0600

    complete design

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="customStyle.css">
+        <link rel="icon" type="image/x-icon" href="https://cdn.edokan.co/public/stores/6206a1b28b069222014ca1ce/icon/Orange Online Shop Modern Logo (1).ico">
+        <title>About Us - Pocket Solution BD</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossorigin>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;700&amp;display=swap">
+        <link rel="stylesheet" media="print" onload="this.media='all'" href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;700&amp;display=swap">
+        <noscript><link rel="stylesheet"></noscript>
+        <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Roboto+Serif:wght@400;700&amp;display=swap">
+        <link rel="stylesheet" media="print" onload="this.media='all'" href="https://fonts.googleapis.com/css2?family=Roboto+Serif:wght@400;700&amp;display=swap">
+        <noscript><link href="https://fonts.googleapis.com/css2?family=Roboto+Serif:wght@400;700&amp;display=swap" rel="stylesheet"></noscript>
+
+        <style>*:focus{
+        outline:none !important;
+        }</style>
+<style>body{font-family: 'Roboto Serif', sans-serif;}
+h1,h2,h3,h4,h5,h6{font-family: 'Roboto Slab', sans-serif;}
+.badge-primary,
+    .btn-primary, .btn-primary:hover, .btn-primary:active, .btn-primary:visited,  .btn-primary:disabled, .btn-primary:focus, .btn-primary:active:focus,
+    .btn-primary:not(:disabled):not(.disabled):active,
+    .btn-outline-primary:not(:disabled):not(.disabled):active, .btn-outline-primary:hover {background-color:  #0a0a0a; border-color: #0a0a0a}
+    a,a:hover,a:active,a:focus,.btn-link{color: #0a0a0a }
+.btn-outline-primary{color: #0a0a0a; border-color: #0a0a0a;}
+.custom-control-input:checked~.custom-control-label::before{ border-color: #0a0a0a; background-color: #0a0a0a }
+nav.navbar{background:#000000}
+body{background:#ffffff}
+footer{background:#080808}
+.card-footer{background:#0a0a0a01}
+label.btn.active .image-control { border: solid 2px #0a0a0a}
+
+.btn-outline-primary {
+    color: #ffffff;
+    border-color: #000000;
+    background-color: #000000;
+    font-size: 13px !important;
+}
+.btn-outline-primary:hover {
+    border-color: #0c0c0c;
+    background-color: #050505;   
+}
+aside#price-filter {
+    display: none;
+}
+#navbar-row-1 {
+    padding-top: 7px;
+}
+
+.badge-primary{
+    background-color: #023139 !important;
+   border-color: #023139 !important;
+}
+#mobile-navbar {
+    padding-bottom: 5px;
+}
+@media (max-width: 767px){
+.navbar-dark .navbar-brand {
+    color: rgb(245 241 241 / 90%);
+    padding-bottom: 20px;
+    padding-top: 20px;
+}
+}
+/* .text-dark {
+    color: #ffffff!important;
+} */
+.btn-primary{
+    background-color: #002333;
+    border-color: #002333;
+}
+.navbar-dark .navbar-nav .nav-link {
+    color: rgb(255 255 255 / 92%);
+}
+
+/*Begin slides page */
+
+@import url('https://fonts.maateen.me/kalpurush/font.css');
+    </style>
+        <link rel="preload" as="script" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.bundle.min.js">
+        <link href="https://fonts.maateen.me/kalpurush/font.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.edokan.dev/al-amin-irfa/font.css">
+    </head>
+    <body class="page-single">
+
+        <div class="row">
+            <div class="col">	
+		
+		<div class="content" id="page-content">
+
+		<!-- Page Content -->
+
+		<?php echo file_get_contents('landing.html') ?>
+
+		<!-- Page Content END -->
+
+            </div>
+        </div>
+        </div>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
